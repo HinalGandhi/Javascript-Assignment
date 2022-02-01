aad417eb552628429d8c6ac859c84e108cdf67eb
“Hinal <hinal.g@simformsolutions.com>
2022-02-01 15:58:48 +0530

    fixed gui

--- index.js
+++ index.js
@@ -1,303 +1,295 @@
-"use strict";
-$(".trigo-title").click(function () {
-  $(".trigo-functions").toggle();
-  if ($(".down1").hasClass("fa-angle-down")) {
-    $(".down1").removeClass("fa-angle-down").addClass("fa-angle-up");
-  } else {
-    $(".down1").addClass("fa-angle-down").removeClass("fa-angle-up");
-  }
-});
-$(".function-title").click(function () {
-  $(".functions").toggle();
-  if ($(".down").hasClass("fa-angle-down")) {
-    $(".down").removeClass("fa-angle-down").addClass("fa-angle-up");
-  } else {
-    $(".down").addClass("fa-angle-down").removeClass("fa-angle-up");
-  }
-});
 let display = document.getElementById("output");
 let buttons = document.getElementsByClassName("btn");
 Array.prototype.forEach.call(buttons, function (button) {
-  button.addEventListener("click", function () {
-    if (
-      button.innerHTML != "=" &&
-      button.innerHTML != "C" &&
-      button.innerHTML != "x" &&
-      button.innerHTML != "÷" &&
-      button.innerHTML != "1/x" &&
-      button.innerHTML !=
-      '<i class="fa fa-square-root-alt" aria-hidden="true"></i>' &&
-      button.innerHTML != "x<sup>2</sup>" &&
-      button.innerHTML != "2<sup>nd</sup>" &&
-      button.innerHTML != "10<sup>x</sup>" &&
-      button.innerHTML != "%" &&
-      button.innerHTML != "|x|" &&
-      button.innerHTML != "e" &&
-      button.innerHTML != "mod" &&
-      button.innerHTML !=
-      '<i class="fa fa-backspace" aria-hidden="true"></i>' &&
-      button.innerHTML != "+/-" &&
-      button.innerHTML != "sin" &&
-      button.innerHTML != "cos" &&
-      button.innerHTML != "tan" &&
-      button.innerHTML != "Floor" &&
-      button.innerHTML != "Ceil" &&
-      button.innerHTML != "Random" &&
-      button.innerHTML != "log" &&
-      button.innerHTML != "ln" &&
-      button.innerHTML != "x<sup>y</sup>" &&
-      button.innerHTML != "n!" &&
-      button.innerHTML != "π" &&
-      button.innerHTML != "exp"
-    ) {
-      display.value += button.innerHTML;
-    } else if (button.innerHTML === "=") {
-      equals();
-    } else if (button.innerHTML === "C") {
-      clear();
-    } else if (button.innerHTML === "2<sup>nd</sup>") {
-      power();
-    } else if (button.innerHTML === "10<sup>x</sup>") {
-      power10();
-    } else if (button.innerHTML === "x") {
-      multiply();
-    } else if (button.innerHTML === "|x|") {
-      absolute();
-    } else if (button.innerHTML === "÷") {
-      divide();
-    } else if (button.innerHTML === "Floor") {
-      floor();
-    } else if (button.innerHTML === "Ceil") {
-      ceil();
-    } else if (button.innerHTML === "Random") {
-      random();
-    } else if (button.innerHTML === "+/-") {
-      plusMinus();
-    } else if (button.innerHTML === "1/x") {
-      upon();
-    } else if (
-      button.innerHTML === '<i class="fa fa-backspace" aria-hidden="true"></i>'
-    ) {
-      backspace();
-    } else if (button.innerHTML === "%") {
-      percent();
-    } else if (button.innerHTML === "π") {
-      pi();
-    } else if (button.innerHTML === "x<sup>2</sup>") {
-      square();
-    } else if (
-      button.innerHTML ===
-      '<i class="fa fa-square-root-alt" aria-hidden="true"></i>'
-    ) {
-      squareRoot();
-    } else if (button.innerHTML === "e") {
-      eulersNum();
-    } else if (button.innerHTML === "mod") {
-      mod();
-    } else if (button.innerHTML === "sin") {
-      sin();
-    } else if (button.innerHTML === "cos") {
-      cos();
-    } else if (button.innerHTML === "tan") {
-      tan();
-    } else if (button.innerHTML === "log") {
-      log();
-    } else if (button.innerHTML === "ln") {
-      ln();
-    } else if (button.innerHTML === "x<sup>y</sup>") {
-      exponent();
-    } else if (button.innerHTML === "n!") {
-      factorial();
-    } else if (button.innerHTML === "exp") {
-      exp();
-    }
-  });
+    button.addEventListener("click", function () {
+        if (button.textContent != "=" &&
+            button.textContent != "C" &&
+            button.textContent != "x" &&
+            button.textContent != "÷" &&
+            button.textContent != "1/x" &&
+            button.textContent !=
+                '√' &&
+            button.textContent !=
+                '3√' &&
+            button.textContent != "x2" &&
+            button.textContent != "2n" &&
+            button.textContent != "x3" &&
+            button.textContent != "10x" &&
+            button.textContent != "%" &&
+            button.textContent != "|x|" &&
+            button.textContent != "e" &&
+            button.textContent != "mod" &&
+            button.textContent !=
+                '⌫' &&
+            button.textContent != "+/-" &&
+            button.textContent != "log" &&
+            button.textContent != "ln" &&
+            button.textContent != "xy" &&
+            button.textContent != "n!" &&
+            button.textContent != "π" &&
+            button.textContent != "exp" &&
+            button.textContent != "2nd") {
+            display.value += button.textContent;
+        }
+        else if (button.textContent === "=") {
+            equals();
+        }
+        else if (button.textContent === "C") {
+            clear();
+        }
+        else if (button.textContent === "2n") {
+            power();
+        }
+        else if (button.textContent === "10x") {
+            power10();
+        }
+        else if (button.textContent === "x") {
+            multiply();
+        }
+        else if (button.textContent === "|x|") {
+            absolute();
+        }
+        else if (button.textContent === "÷") {
+            divide();
+        }
+        else if (button.textContent === "+/-") {
+            plusMinus();
+        }
+        else if (button.textContent === "1/x") {
+            upon();
+        }
+        else if (button.textContent === '⌫') {
+            backspace();
+        }
+        else if (button.textContent === "%") {
+            percent();
+        }
+        else if (button.textContent === "π") {
+            pi();
+        }
+        else if (button.textContent === "x2") {
+            square();
+        }
+        else if (button.textContent === "x3") {
+            cube();
+        }
+        else if (button.textContent ===
+            '√') {
+            squareRoot();
+        }
+        else if (button.textContent ===
+            '3√') {
+            cuberoot();
+        }
+        else if (button.textContent === "e") {
+            eulersNum();
+        }
+        else if (button.textContent === "mod") {
+            mod();
+        }
+        else if (button.textContent === "log") {
+            log();
+        }
+        else if (button.textContent === "ln") {
+            ln();
+        }
+        else if (button.textContent === "xy") {
+            exponent();
+        }
+        else if (button.textContent === "n!") {
+            factorial();
+        }
+        else if (button.textContent === "exp") {
+            exp();
+        }
+    });
 });
-
-function checkLength() {
-  if (display.value.length >= 23) {
-    display.value = "Overload Error";
-  }
-}
-function syntaxError() {
-  if (eval(display.value) == SyntaxError) {
-    display.value = "Syntax Error";
-  }
-}
 function equals() {
-  if (display.value.indexOf("^") > -1) {
-    let base = display.value.slice(0, display.value.indexOf("^"));
-    let exponent = display.value.slice(display.value.indexOf("^") + 1);
-    display.value = eval("Math.pow(" + base + "," + exponent + ")");
-  } else if (display.value === "" || display.value === undefined) {
-    clear();
-  } else {
-    display.value = eval(display.value);
-    checkLength();
-    syntaxError();
-  }
+    if (display.value.indexOf("^") > -1) {
+        let base = display.value.slice(0, display.value.indexOf("^"));
+        let exponent_1 = display.value.slice(display.value.indexOf("^") + 1);
+        display.value = eval("Math.pow(" + base + "," + exponent_1 + ")");
+    }
+    else if (display.value === "" || display.value === undefined) {
+        clear();
+    }
+    else {
+        try {
+            let x = eval(display.value);
+            display.value = eval(x);
+        }
+        catch (e) {
+            display.value = "Syntax error!";
+        }
+    }
 }
 function clear() {
-  display.value = "";
+    display.value = "";
 }
 function backspace() {
-  display.value = display.value.substring(0, display.value.length - 1);
+    display.value = display.value.substring(0, display.value.length - 1);
 }
 function multiply() {
-  display.value = display.value + "*";
+    display.value = display.value + "*";
 }
 function divide() {
-  display.value = display.value + "/";
+    display.value = display.value + "/";
 }
 function plusMinus() {
-  if (display.value.charAt(0) === "-") {
-    display.value = display.value.slice(1);
-  } else {
-    display.value = "-" + display.value;
-  }
+    if (display.value.charAt(0) === "-") {
+        display.value = display.value.slice(1);
+    }
+    else {
+        display.value = "-" + display.value;
+    }
 }
 function factorial() {
-  let result = 1;
-  if (display.value === 0) {
-    display.value = "1";
-  } else if (display.value < 0) {
-    display.value = "undefined";
-  } else {
-    let result = 1;
-    for (let i = display.value; i > 0; i--) {
-      result = result * i;
+    let fact = 1;
+    let value = Number(display.value);
+    for (let i = 1; i <= value; i++) {
+        fact = fact * i;
+        display.value = String(fact);
     }
-    display.value = result;
-  }
 }
 function pi() {
-  if (display.value === "") {
-    display.value = Math.PI;
-  } else {
-    display.value = display.value * Math.PI;
-  }
+    display.value = String(Number(display.value) * Math.PI);
 }
 function square() {
-  display.value = display.value * display.value;
+    display.value = String(Math.pow(Number(display.value), 2));
+}
+function cube() {
+    display.value = String(Math.pow(Number(display.value), 3));
 }
 function squareRoot() {
-  display.value = Math.sqrt(display.value);
+    display.value = String(Math.sqrt(Number(display.value)));
+}
+function cuberoot() {
+    display.value = String(Math.cbrt(Number(display.value)));
 }
 function percent() {
-  display.value = display.value / 100;
+    display.value = String(Number(display.value) / 100);
 }
 function sin() {
-  display.value = Math.sin(display.value);
+    display.value = String(Math.sin(Number(display.value)));
 }
 function cos() {
-  display.value = Math.cos(display.value);
+    display.value = String(Math.cos(Number(display.value)));
 }
 function tan() {
-  display.value = Math.tan(display.value);
+    display.value = String(Math.tan(Number(display.value)));
 }
 function log() {
-  display.value = Math.log10(display.value);
+    display.value = String(Math.LOG10E);
 }
 function ln() {
-  display.value = Math.log(display.value);
+    display.value = String(Math.log(Number(display.value)));
 }
 function exponent() {
-  display.value = display.value + "^";
+    display.value += "^";
 }
 function exp() {
-  display.value = Math.exp(display.value);
+    display.value = String(Math.exp(Number(display.value)));
 }
 function degtorad() {
-  if ($(".degrees").text() == "DEG") {
-    display.value = display.value * (180 / Math.PI);
-    $(".degrees").text("RAD");
-  } else {
-    display.value = display.value * (Math.PI / 180);
-    $(".degrees").text("DEG");
-  }
+    if ($(".degrees").text() == "DEG") {
+        display.value = String((Math.PI * Number(display.value)) / 180);
+        $(".degrees").text("RAD");
+    }
+    else {
+        display.value = String((180 * Number(display.value)) / Math.PI);
+        $(".degrees").text("DEG");
+    }
 }
 function power() {
-  display.value = Math.pow(2, display.value);
+    display.value = String(Math.pow(2, Number(display.value)));
 }
 function power10() {
-  display.value = Math.pow(10, display.value);
+    display.value = String(Math.pow(10, Number(display.value)));
 }
 function eulersNum() {
-  if (display.value) {
-    display.value = Math.E * display.value;
-  } else {
-    display.value = Math.E;
-  }
+    if (display.value) {
+        display.value = String(Math.E * Number(display.value));
+    }
+    else {
+        display.value = String(Math.E);
+    }
 }
 function upon() {
-  if (display.value == 0 && display.value == "") {
-    display.value = "Error";
-  } else {
-    display.value = 1 / display.value;
-  }
+    display.value = (1 / Number(display.value)).toFixed(2);
 }
 function absolute() {
-  display.value = Math.abs(display.value);
+    display.value = String(Math.abs(Number(display.value)));
 }
 function mod() {
-  display.value = display.value + "%";
+    display.value = display.value + "%";
 }
 function ceil() {
-  display.value = Math.ceil(display.value);
+    display.value = String(Math.ceil(Number(display.value)));
 }
 function floor() {
-  display.value = Math.floor(display.value);
+    display.value = String(Math.floor(Number(display.value)));
 }
 function random() {
-  display.value = Math.random();
+    display.value = String((Math.random() * 10).toFixed(5));
 }
 function exponential() {
-  let num = parseFloat(display.value);
-  display.value = num.toExponential();
+    let num = parseFloat(display.value);
+    display.value = num.toExponential();
 }
 let memory = [];
 function mplus() {
-  if (display.value != "") {
-    $(".memoryplus").css("display", "block");
-    $(".memoryminus").css("display", "none");
-    $(".memory").css("display", "none");
-    memory.push(parseInt(display.value));
-    display.value = memory.reduce((a, b) => a + b, 0);
-  }
+    if (display.value != "") {
+        $(".memoryplus").css("display", "block");
+        $(".memoryminus").css("display", "none");
+        $(".memory").css("display", "none");
+        memory.push(parseInt(display.value));
+        display.value = memory.reduce(function (a, b) { return a + b; }, 0);
+    }
 }
 function mminus() {
-  if (display.value != "") {
-    $(".memoryminus").css("display", "block");
-    $(".memoryplus").css("display", "none");
-    $(".memory").css("display", "none");
-    memory.push(parseInt(-display.value));
-    display.value = memory.reduce((a, b) => a + b, 0);
-  }
+    if (display.value != "") {
+        $(".memoryminus").css("display", "block");
+        $(".memoryplus").css("display", "none");
+        $(".memory").css("display", "none");
+        memory.push(parseInt(display.value));
+        display.value = memory.reduce(function (a, b) { return a - b; }, 0);
+    }
 }
 function mrecall() {
-  if (memory.length > 0) {
-    $(".memory").css("display", "block");
-    $(".memoryminus").css("display", "none");
-    $(".memoryplus").css("display", "none");
-    if (memory.length > 1) {
-      display.value = memory[memory.length - 1];
-    } else {
-      display.value = memory[0];
+    if (memory.length > 0) {
+        $(".memory").css("display", "block");
+        $(".memoryminus").css("display", "none");
+        $(".memoryplus").css("display", "none");
+        if (memory.length > 1) {
+            display.value = memory[memory.length - 1];
+        }
+        else {
+            display.value = memory[0];
+        }
     }
-  }
 }
 function mclear() {
-  memory = [];
-  $(".memory").css("display", "none");
-  $(".memoryminus").css("display", "none");
-  $(".memoryplus").css("display", "none");
-  display.value = "";
+    memory = [];
+    $(".memory").css("display", "none");
+    $(".memoryminus").css("display", "none");
+    $(".memoryplus").css("display", "none");
+    display.value = "";
 }
 function msave() {
-  if (display.value != "") {
-    $(".memoryplus").css("display", "none");
-    $(".memoryminus").css("display", "none");
-    $(".memory").css("display", "block");
-    memory.push(parseInt(display.value));
-  }
+    if (display.value != "") {
+        $(".memoryplus").css("display", "none");
+        $(".memoryminus").css("display", "none");
+        $(".memory").css("display", "block");
+        memory.push(parseInt(display.value));
+    }
 }
+let btnswap = function () {
+    if ($(".sqaure").is(":visible")) {
+        $('.sqaure').css('display', "none");
+        $('.cube').css('display', 'block');
+    }
+    else {
+        $('.sqaure').css('display', "block");
+        $('.cube').css('display', 'none');
+    }
+};
